c5f72794e2a0d4b6feb3accd2eb4e62a3f7fe4c4
Muhammad Rizal Pahlevi <mrizalpahlevi372@gmail.com>
2021-01-03 18:44:09 +0700

    Initial commit :tada:

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Company Profile</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <nav>
+        <div class="wrap">
+            <div class="title">WebsiteKU</div>
+            <div class="menu">
+                <ul>
+                    <li><a href="index.php?page=home"></a></li>
+                    <li><a href="index.php?page=about"></a></li>
+                    <li><a href="index.php?page=future"></a></li>
+                    <li><a href="index.php?page=contact"></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+
+
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
